5527e0bf0bd02d6718c9c8f246b7f334e6e2fb60
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2023-08-20 17:40:20 -0400

    actually run the server in the script

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+./server &
 ../proxy -cert "/etc/letsencrypt/live/$HOSTNAME/fullchain.pem" -key "/etc/letsencrypt/live/$HOSTNAME/privkey.pem" -from 0.0.0.0:4430 -to 127.0.0.1:8080
