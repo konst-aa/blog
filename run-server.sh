513f312166d68f90d7a661628171094b43c91203
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2023-08-20 17:54:03 -0400

    improve run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+source env.sh
 ./server &
-../proxy -cert "/etc/letsencrypt/live/$HOSTNAME/fullchain.pem" -key "/etc/letsencrypt/live/$HOSTNAME/privkey.pem" -from 0.0.0.0:4430 -to 127.0.0.1:8080
+../proxy -cert "/etc/letsencrypt/live/$ADDRESS/fullchain.pem" -key "/etc/letsencrypt/live/$ADDRESS/privkey.pem" -from 0.0.0.0:4430 -to 127.0.0.1:8080
