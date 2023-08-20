e7fad21be0a4a5aeceafbb17197aa7116a7e535c
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2023-08-20 17:56:51 -0400

    use proper http port

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source env.sh
 ./server &
-../proxy -cert "/etc/letsencrypt/live/$ADDRESS/fullchain.pem" -key "/etc/letsencrypt/live/$ADDRESS/privkey.pem" -from 0.0.0.0:4430 -to 127.0.0.1:8080
+../proxy -cert "/etc/letsencrypt/live/$ADDRESS/fullchain.pem" -key "/etc/letsencrypt/live/$ADDRESS/privkey.pem" -from 0.0.0.0:443 -to 127.0.0.1:8080
