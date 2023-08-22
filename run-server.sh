0b8e24f59e1e89591bfefacce29622957b97b95b
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2023-08-21 21:23:09 -0400

    making sudo inherit path

--- run-server.sh
+++ run-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source env.sh
 ./server &
-ssl-proxy -cert "/etc/letsencrypt/live/$ADDRESS/fullchain.pem" -key "/etc/letsencrypt/live/$ADDRESS/privkey.pem" -from 0.0.0.0:443 -to 127.0.0.1:8080 &
+sudo env "PATH=$PATH" ssl-proxy -cert "/etc/letsencrypt/live/$ADDRESS/fullchain.pem" -key "/etc/letsencrypt/live/$ADDRESS/privkey.pem" -from 0.0.0.0:443 -to 127.0.0.1:8080 &
 disown -a
