e2090af526eb104850a2d41abb02255162205a44
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2023-08-31 10:37:33 -0400

    update emails script

--- send-emails.sh
+++ send-emails.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SUBJECT="Subject: new blog post just dropped!\nContent-Type: text/html\n\n"
+#SUBJECT="Subject: new blog post just dropped!\nContent-Type: text/html\n\n"
 UNSUBCRIBE="to unsubscribe, click this <a href=\"ka.dreadmaw.industries/unsubscribe.html\">link</a>"
 CONTENT=$(cat -)
 ARR=("msmtp" "-a" "gmail")
@@ -7,4 +7,4 @@ echo "sending emails..."
 for EMAIL in $(cat emails.txt); do
     ARR+=( $EMAIL )
 done
-printf "$SUBJECT$content\n\n$UNSUBCRIBE" | xargs -a <(cat emails.txt) msmtp -a gmail
+printf "$content\n\n$UNSUBCRIBE" | xargs -a <(cat emails.txt) msmtp -a gmail
